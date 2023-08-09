a29f35fa1d2db0e2306894366a4acf4fdeb158b4
mide7 <imharyomide@gmail.com>
2023-08-08 23:38:45 -0700

    seo: og and twitter meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,31 @@ export const metadata: Metadata = {
     "Experienced software engineer showcasing a portfolio of creative and responsive web projects. Specializing in user-centric design and modern frontend technologies.",
   keywords:
     "Frontend developer, Responsive web design, User experience (UX), User interface (UI), HTML5, CSS3, JavaScript, ReactJS, Web animations, Cross-browser compatibility, Mobile-friendly design, Web accessibility, Interactive web design, Backend developer, Node.js, Express.js, RESTful APIs,  SQL, MongoDB, Authentication & authorization, API integration, Server deployment, Web security, Performance optimization, Web design portfolio, Creative web projects,  Web development showcase, Web design showcase, Modern web technologies, Creative coding,  User-centric design, Innovative web projects, Interactive websites, Responsive UI, Frontend frameworks, Backend frameworks, Web app development, HTML/CSS expertise",
+  openGraph: {
+    title: "Ayomide Osunsami | Software Engineer",
+    description:
+      "Experienced software engineer showcasing a portfolio of creative and responsive web projects. Specializing in user-centric design and modern frontend technologies.",
+    url: "https://ayomide-osunsami.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "/public/images/logo_icon.png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Ayomide Osunsami | Software Engineer",
+    description:
+      "Experienced software engineer showcasing a portfolio of creative and responsive web projects. Specializing in user-centric design and modern frontend technologies.",
+    // url: "https://ayomide-osunsami.vercel.app/",
+    // type: "website",
+    images: [
+      {
+        url: "/public/images/logo_icon.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
